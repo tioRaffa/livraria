29c2b655c73fcced3f6a46b9a11ac1a36c9c9cbc
Rafael Muniz <rafaelmuniz200@gmail.com>
2025-03-23 10:18:09 -0300

    Implement book detail view and template; remove unused index page; update navbar and head links

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-APP_NAME = 'library'
+app_name = 'library'
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('detail/<int:id>', views.DetailBook.as_view(), name='detail_book')
 ]
--- library/views.py
+++ library/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import View, ListView
+from django.views.generic import View, ListView, DetailView
 from .models import BookModel
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 class HomeView(ListView):
     model = BookModel
@@ -9,3 +11,12 @@ class HomeView(ListView):
     context_object_name = 'books'
     
     paginate_by = 4
+    
+    
+class DetailBook(DetailView):
+    model = BookModel
+    template_name = 'pages/detail_page.html'
+    context_object_name = 'book'
+    
+    def get_object(self, queryset=None):
+        return get_object_or_404(BookModel, id=self.kwargs['id'], is_published=True)
